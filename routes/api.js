9af469f913f99509b63b017087f69a464a98d7cf
Muhammad Nouman <110379750+nomiwsd@users.noreply.github.com>
2023-09-07 22:52:43 +0500

    adding code in api.js and server.js

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,84 @@
 'use strict';
+const { parse } = require("dotenv");
+const mongoose=require("mongoose");
+const objectId=mongoose.Types.ObjectId;
+const request= require('request-promise-native');
+
+const stockSchema=new mongoose.Schema(
+  {code:String,
+  likes:{type:[String],default:[]}
+  })
+
+  const stock=mongoose.model('stock',stockSchema);
+
+  function saveStock(code,like,ip){
+    return stock.findOne({code:code})
+    .then(
+      stock=>{
+        if(!stock){
+          let newStock=new Stock ({code:code,likes:like ?[ip]:[]});
+          return newStock.save()
+        }
+        else{
+          if(like && stock.likes.indexOf(ip)===-1){
+            stock.likes.push(ip)
+          }
+          return stock.save();
+        }
+      }
+    )
+  }
+
+function parseData(data){
+   let i=0;
+   let stockData=[];
+   let likes=[];
+   while(i<data.lenght){
+    let stock={
+      stock:data[i].code,price:parseFloat(data[i+1])
+    }
+    likes.push(data[i].likes.lenght);
+    stockData.push(stock);
+    i += 2;
+   }
+   if(likes.length>1){
+    stockData[0].rel_likes=likes[0]-likes[1];
+    stockData[1].rel_likes=likes[1]-likes[0];
+   }
+   else{
+    stockData[0].likes=likes[0];
+    stockData=stockData[0];
+   }
+   return stockData;
+}
 
 module.exports = function (app) {
+app.get('/api/testing',(req,res)=>{
+  res.json({IP:req.ip})
+})
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      let code=req.query.stock ||'';
+      if(!Array.isArray(code)){
+        code=[code];
+      }
+      let promises=[];
+      code.forEach(code=>{
+        promises.push(saveStock(code.toUpperCase(),req.query.like,req.ip));
+        let url='';
+        promises.push(request(url))
+      }
+      )
+      Promise.all(promises)
+      .then(data=>{
+        let stockData=parseData(data);
+        res.json({stockData})
+      })
+      .catch(err=>{
+        console.log(err);
+        res.send(err);
+      })
     });
     
 };
--- server.js
+++ server.js
@@ -1,51 +1,75 @@
-'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
+const mongodb = require("mongodb");
+const mongoose = require("mongoose");
+const helmet=require('helmet');
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({directives :{
+  defaultSrc:["'self"],
+  scriptSrc:["'self'"],
+  styleSrc:["'self'"]
+}})) 
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+const URI = process.env.DB_URL;
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+  // Your code here
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  console.log("Your app is listening on port " + listener.address().port);
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
-        console.log('Tests are not valid:');
+      } catch (e) {
+        console.log("Tests are not valid:");
         console.error(e);
       }
     }, 3500);
